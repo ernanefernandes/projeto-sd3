531c6b6d49368bf5cacd51720f4dd41b3b3e0f02
ernanefernandes <ernane.fernandes@gmail.com>
2021-05-05 12:21:22 -0300

    Static charts container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,23 +10,23 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary py-3"> DashBoard de Vendas </h1>
-      </div>
-      <div className="row px-3">
-        <div className="col-sm-6">
-          <h5 className="text-center text-secundary">Taxa de sucesso %  </h5>
-          <BarChart />
-        </div>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Taxa de sucesso %  </h5>
+            <BarChart />
+          </div>
 
-        <div className="col-sm-6">
-          <h5 className="text-center text-secundary"> Participação nas vendas </h5>
-          <DonutChart />
-        </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary"> Participação nas vendas </h5>
+            <DonutChart />
+          </div>
 
+        </div>
+        <div className="py-3">
+          <h5 className="text-primary"> Todas  vendas </h5>
+        </div>
+        <DataTable />
       </div>
-      <div className="py-3">
-        <h5 className="text-primary"> Todas  vendas </h5>
-      </div>
-      <DataTable />
       <Footer />
     </>
   );
