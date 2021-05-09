70a491ca0b219879d885631d2faf3dc043ef9e1f
ernanefernandes <ernane.fernandes@gmail.com>
2021-05-09 08:36:46 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <h1>Pagina Home </h1>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
